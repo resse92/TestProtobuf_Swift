36c2e0da8ad99281065668fae9781d10d5cef7ff
chenmu <chenmu@xiaohongshu.com>
2023-03-21 13:47:22 +0800

    Initial Commit

---
+++ TestProtobuf/TestProtobufApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProtobufApp.swift
+//  TestProtobuf
+//
+//  Created by chenmu on 2023/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TestProtobufApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
